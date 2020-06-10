4f8bae55e1b9c51aa69ca9a66d34fb49ae5d52ac
Aurelien Chaudagne <ouralien@gmail.com>
2020-06-10 16:26:20 -0700

    Fix

--- squarego/Item.go
+++ squarego/Item.go
@@ -2,9 +2,10 @@ package squarego
 
 // Item ...
 type Item struct {
-	UID            *string       `json:"uid"`
-	Name           *string       `json:"name"`
-	Quantity       *string       `json:"quantity"`
-	BasePriceMoney PaymentAmount `json:"base_price_money"`
-	TotalMoney     PaymentAmount `json:"total_money"`
+	UID             *string       `json:"uid"`
+	CatalogObjectID *string       `json:"catalog_object_id"`
+	Name            *string       `json:"name"`
+	Quantity        *string       `json:"quantity"`
+	BasePriceMoney  PaymentAmount `json:"base_price_money"`
+	TotalMoney      PaymentAmount `json:"total_money"`
 }
